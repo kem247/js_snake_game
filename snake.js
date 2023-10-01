0fc61ccae81f694242915dbba66319ea33bcb41b
Kemi Thomas <kemothom71@gmail.com>
2023-09-30 20:26:28 -0400

    snake.js

--- snake.js
+++ snake.js
@@ -1,4 +1,11 @@
 const base = require('./base')
+Object.getOwnPropertyNames(base).map(p => global[p] = base[p])
+
+// Constants
+const NORTH = { x: 0, y: -1}
+const EAST = { x: 1, y: 0 }
+const WEST = { x: -1, y: 0 }
+const SOUTH = { x:0, y: 1 }
 
 // Initial State
 const initialState = () => ({
@@ -9,11 +16,10 @@ const initialState = () => ({
     apple: { x: 16, y: 2},
 })
 
-// Constants
-const NORTH = { x: 0, y: -1}
-const EAST = { x: 1, y: 0 }
-const WEST = { x: -1, y: 0 }
-const SOUTH = { x:0, y: 1 }
+const enqueue = (state, move) => validMove(move)(state)
+? merge(state)({ moves: state.moves.concat([move])}) : 
+state
+
 
 // Randomness
 const rndPos = table => ({
--- web.js
+++ web.js
@@ -4,6 +4,25 @@ const ctx = canvas.getContext('2d')
 // Mutable state 
 let state = initialState()
 
+// Position helpers
+const x = c => Math.round(c * canvas.width / state.cols)
+const y = r => Math.round(r * canvas.width / state.rows)
+
+// Game loop draw
+const draw = () => {
+// clear
+    ctx.fillStyle = '#232323'
+    ctx.fillRect(0, 0, canvas.width, canvas.height )
+
+// draw snake 
+    ctx.fillStyle = 'rgb(0,200,50)'  // x pos, y pos, width of 1 tile, height of 1 tile
+    state.snake.map(p => ctx.fillRect(x(p.x), y(p.y), x(1), y(1)))
+
+// draw apples
+    ctx.fillStyle = 'rgb(255,50,0)'
+    ctx.fillRect(x(state.apple.x), y(state.apple.y), x(1), y(1))
+}
+
 
 // Game loop update
 const step = timeStamp1 => timeStamp2 => {
