7aef847c481d5fcc078cc85df94e21eb2e4855dd
Kemi Thomas <kemothom71@gmail.com>
2023-10-07 16:02:17 -0400

    base.js functions

--- base.js
+++ base.js
@@ -1,5 +1,10 @@
 
 const dropFirst = xs => xs.slice(1)
+const dropLast = xs = xs.slice(0, xs.length-1)
 const spec = o => x => Object.keys(0)
     .map(k => objOf(k)(o[k](x))) // cpnstructs a new object of that key and constructs a function 
     .reduce((acc, o) => Object.assign(acc, o))
+const id = x => x
+const k = x => y => x
+const map = f => xs => xs.map(f)
+const mapi = f => xs => xs.map((x,i) => f((x)(i)))
